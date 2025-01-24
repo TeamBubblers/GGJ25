8b40dae805c79c7f89d82f384b7708083332951f
DonAntti <48753440+DonAntti@users.noreply.github.com>
2025-01-25 01:35:15 +0200

    Player prototype
    Player prototype Movement, Jump & shooting.

---
+++ GGJ25/Assets/Scripts/AmmoMovement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoMovement : MonoBehaviour
+{
+
+    private float lifeTime;
+
+    [SerializeField]
+    private float maxLifeTime;
+
+    [SerializeField]
+    private float ammoSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lifeTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lifeTime += Time.deltaTime;
+
+        transform.Translate(transform.right * ammoSpeed * Time.deltaTime, Space.World);
+
+        if(lifeTime >= maxLifeTime)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Platform" || other.tag == "Enemy")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ GGJ25/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,127 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float jumpPower;
+
+    private float moveX;
+
+    [SerializeField]
+    private Rigidbody rb;
+
+    private bool grounded;
+
+    [SerializeField]
+    private GameObject ammoSpawnpointFront;
+
+    [SerializeField]
+    private GameObject ammoSpawnpointUp;
+
+    [SerializeField]
+    private GameObject ammoSpawnpointDown;
+
+    [SerializeField]
+    private GameObject ammoSpawnpointDownFront;
+
+    [SerializeField]
+    private GameObject ammoSpawnpointUpFront;
+
+    [SerializeField]
+    private GameObject bulletPrefab;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveX = Input.GetAxis("Horizontal");
+
+        if (Input.GetKey(KeyCode.RightArrow) && grounded == true)
+        {
+            rb.velocity = new Vector3(moveX + speed * Time.deltaTime, rb.velocity.y, rb.velocity.z);
+            this.transform.eulerAngles = new Vector3(this.transform.rotation.x, 0, this.transform.rotation.z);
+        }
+
+        else if (Input.GetKey(KeyCode.LeftArrow) && grounded == true)
+        {
+            rb.velocity = new Vector3(moveX - speed * Time.deltaTime, rb.velocity.y, rb.velocity.z);
+            this.transform.eulerAngles = new Vector3(this.transform.rotation.x, 180, this.transform.rotation.z);
+        }
+
+        
+
+        else if (!Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow) && grounded == true)
+        {
+            rb.velocity = new Vector3(0, rb.velocity.y, rb.velocity.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z) && grounded == true || Input.GetKeyDown(KeyCode.Space) && grounded == true)
+        {
+            rb.AddForce(new Vector3(rb.velocity.x, jumpPower, rb.velocity.z));
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.X) && Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.RightArrow)
+            || Input.GetKeyDown(KeyCode.LeftControl) && Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.RightArrow)
+            || Input.GetKeyDown(KeyCode.X) && Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.LeftArrow)
+            || Input.GetKeyDown(KeyCode.LeftControl) && Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.LeftArrow))
+        {
+            GameObject bullet = Instantiate(bulletPrefab, ammoSpawnpointDownFront.transform.position, ammoSpawnpointDownFront.transform.rotation);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.X) && Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.RightArrow)
+            || Input.GetKeyDown(KeyCode.LeftControl) && Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.RightArrow)
+            || Input.GetKeyDown(KeyCode.X) && Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.LeftArrow)
+            || Input.GetKeyDown(KeyCode.LeftControl) && Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.LeftArrow))
+        {
+            GameObject bullet = Instantiate(bulletPrefab, ammoSpawnpointUpFront.transform.position, ammoSpawnpointUpFront.transform.rotation);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.X) && Input.GetKey(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.LeftControl) && Input.GetKey(KeyCode.UpArrow))
+        {
+            GameObject bullet = Instantiate(bulletPrefab, ammoSpawnpointUp.transform.position, ammoSpawnpointUp.transform.rotation);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.X) && Input.GetKey(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.LeftControl) && Input.GetKey(KeyCode.DownArrow))
+        {
+            GameObject bullet = Instantiate(bulletPrefab, ammoSpawnpointDown.transform.position, ammoSpawnpointDown.transform.rotation);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.X) || Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            GameObject bullet = Instantiate(bulletPrefab, ammoSpawnpointFront.transform.position, transform.rotation);
+        }
+
+
+
+    }
+
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Platform")
+        {
+            grounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Platform")
+        {
+            grounded = false;
+        }
+    }
+}
