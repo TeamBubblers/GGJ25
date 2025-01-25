26d8680c84e7af023137b1c1ffd03215001556f9
juumyllyla <31961349+juumyllyla@users.noreply.github.com>
2025-01-25 12:23:06 +0200

    Added player knockback & stun

--- GGJ25/Assets/Scripts/PlayerMovement.cs
+++ GGJ25/Assets/Scripts/PlayerMovement.cs
@@ -50,136 +50,167 @@ public class PlayerMovement : MonoBehaviour
 
     private bool Jumped;
 
+    private bool isStunned;
 
+    [SerializeField]
+    private float totalStunTime;
+    private float currentStunTime;
+
+    [SerializeField]
+    private float knockBackForce;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //currentStunTime = totalStunTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        moveX = Input.GetAxis("Horizontal");
-
-        reloadTimer += Time.deltaTime;
-
-        coyoteTimer += Time.deltaTime;
-
-        if (Input.GetKey(KeyCode.RightArrow) && grounded == true)
+        if (!isStunned)
         {
-            rb.velocity = new Vector3(moveX + speed * Time.deltaTime, rb.velocity.y, rb.velocity.z);
-            this.transform.eulerAngles = new Vector3(this.transform.rotation.x, 0, this.transform.rotation.z);
-        }
+            moveX = Input.GetAxis("Horizontal");
 
-        else if (Input.GetKey(KeyCode.LeftArrow) && grounded == true)
-        {
-            rb.velocity = new Vector3(moveX - speed * Time.deltaTime, rb.velocity.y, rb.velocity.z);
-            this.transform.eulerAngles = new Vector3(this.transform.rotation.x, 180, this.transform.rotation.z);
-        }
+            reloadTimer += Time.deltaTime;
 
-        
+            coyoteTimer += Time.deltaTime;
 
-        else if (!Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow) && grounded == true)
-        {
-            rb.velocity = new Vector3(0, rb.velocity.y, rb.velocity.z);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Z) && grounded == true && Jumped == false || Input.GetKeyDown(KeyCode.Space) && grounded == true && Jumped == false)
-        {
-            rb.AddForce(new Vector3(rb.velocity.x, jumpPower, rb.velocity.z));
-            coyoteTimer = 0;
-            Jumped = true;
-        }
-
-        if(reloadTimer >= reloaded)
-        {
-            if (Input.GetKeyDown(KeyCode.X) && Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.RightArrow)
-             || Input.GetKeyDown(KeyCode.LeftControl) && Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.RightArrow)
-             || Input.GetKeyDown(KeyCode.X) && Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.LeftArrow)
-             || Input.GetKeyDown(KeyCode.LeftControl) && Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.LeftArrow))
+            if (Input.GetKey(KeyCode.RightArrow) && grounded == true)
             {
-                GameObject Ammo = ObjectPool.SharedInstance.GetAmmo();
-                Ammo.transform.position = ammoSpawnpointDownFront.transform.position;
-                Ammo.transform.rotation = ammoSpawnpointDownFront.transform.rotation;
-                Ammo.transform.localScale = transform.localScale / 2;
-                Ammo.SetActive(true);
-                reloadTimer = 0;
+                rb.velocity = new Vector3(moveX + speed * Time.deltaTime, rb.velocity.y, rb.velocity.z);
+                this.transform.eulerAngles = new Vector3(this.transform.rotation.x, 0, this.transform.rotation.z);
             }
 
-            else if (Input.GetKeyDown(KeyCode.X) && Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.RightArrow)
-                || Input.GetKeyDown(KeyCode.LeftControl) && Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.RightArrow)
-                || Input.GetKeyDown(KeyCode.X) && Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.LeftArrow)
-                || Input.GetKeyDown(KeyCode.LeftControl) && Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.LeftArrow))
+            else if (Input.GetKey(KeyCode.LeftArrow) && grounded == true)
             {
-                GameObject Ammo = ObjectPool.SharedInstance.GetAmmo();
-                Ammo.transform.position = ammoSpawnpointUpFront.transform.position;
-                Ammo.transform.rotation = ammoSpawnpointUpFront.transform.rotation;
-                Ammo.transform.localScale = transform.localScale / 2;
-                Ammo.SetActive(true);
-                reloadTimer = 0;
+                rb.velocity = new Vector3(moveX - speed * Time.deltaTime, rb.velocity.y, rb.velocity.z);
+                this.transform.eulerAngles = new Vector3(this.transform.rotation.x, 180, this.transform.rotation.z);
             }
 
-            else if (Input.GetKeyDown(KeyCode.X) && Input.GetKey(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.LeftControl) && Input.GetKey(KeyCode.UpArrow))
+
+
+            else if (!Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow) && grounded == true)
             {
-                GameObject Ammo = ObjectPool.SharedInstance.GetAmmo();
-                Ammo.transform.position = ammoSpawnpointUp.transform.position;
-                Ammo.transform.rotation = ammoSpawnpointUp.transform.rotation;
-                Ammo.transform.localScale = transform.localScale / 2;
-                Ammo.SetActive(true);
-                reloadTimer = 0;
+                rb.velocity = new Vector3(0, rb.velocity.y, rb.velocity.z);
             }
 
-            else if (Input.GetKeyDown(KeyCode.X) && Input.GetKey(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.LeftControl) && Input.GetKey(KeyCode.DownArrow))
+            if (Input.GetKeyDown(KeyCode.Z) && grounded == true && Jumped == false || Input.GetKeyDown(KeyCode.Space) && grounded == true && Jumped == false)
             {
-                GameObject Ammo = ObjectPool.SharedInstance.GetAmmo();
-                Ammo.transform.position = ammoSpawnpointDown.transform.position;
-                Ammo.transform.rotation = ammoSpawnpointDown.transform.rotation;
-                Ammo.transform.localScale = transform.localScale / 2;
-                Ammo.SetActive(true);
-                reloadTimer = 0;
+                rb.AddForce(new Vector3(rb.velocity.x, jumpPower, rb.velocity.z));
+                coyoteTimer = 0;
+                Jumped = true;
+            }
+
+            if (reloadTimer >= reloaded)
+            {
+                if (Input.GetKeyDown(KeyCode.X) && Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.RightArrow)
+                 || Input.GetKeyDown(KeyCode.LeftControl) && Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.RightArrow)
+                 || Input.GetKeyDown(KeyCode.X) && Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.LeftArrow)
+                 || Input.GetKeyDown(KeyCode.LeftControl) && Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.LeftArrow))
+                {
+                    GameObject Ammo = ObjectPool.SharedInstance.GetAmmo();
+                    Ammo.transform.position = ammoSpawnpointDownFront.transform.position;
+                    Ammo.transform.rotation = ammoSpawnpointDownFront.transform.rotation;
+                    Ammo.transform.localScale = transform.localScale / 2;
+                    Ammo.SetActive(true);
+                    reloadTimer = 0;
+                }
+
+                else if (Input.GetKeyDown(KeyCode.X) && Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.RightArrow)
+                    || Input.GetKeyDown(KeyCode.LeftControl) && Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.RightArrow)
+                    || Input.GetKeyDown(KeyCode.X) && Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.LeftArrow)
+                    || Input.GetKeyDown(KeyCode.LeftControl) && Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.LeftArrow))
+                {
+                    GameObject Ammo = ObjectPool.SharedInstance.GetAmmo();
+                    Ammo.transform.position = ammoSpawnpointUpFront.transform.position;
+                    Ammo.transform.rotation = ammoSpawnpointUpFront.transform.rotation;
+                    Ammo.transform.localScale = transform.localScale / 2;
+                    Ammo.SetActive(true);
+                    reloadTimer = 0;
+                }
+
+                else if (Input.GetKeyDown(KeyCode.X) && Input.GetKey(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.LeftControl) && Input.GetKey(KeyCode.UpArrow))
+                {
+                    GameObject Ammo = ObjectPool.SharedInstance.GetAmmo();
+                    Ammo.transform.position = ammoSpawnpointUp.transform.position;
+                    Ammo.transform.rotation = ammoSpawnpointUp.transform.rotation;
+                    Ammo.transform.localScale = transform.localScale / 2;
+                    Ammo.SetActive(true);
+                    reloadTimer = 0;
+                }
+
+                else if (Input.GetKeyDown(KeyCode.X) && Input.GetKey(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.LeftControl) && Input.GetKey(KeyCode.DownArrow))
+                {
+                    GameObject Ammo = ObjectPool.SharedInstance.GetAmmo();
+                    Ammo.transform.position = ammoSpawnpointDown.transform.position;
+                    Ammo.transform.rotation = ammoSpawnpointDown.transform.rotation;
+                    Ammo.transform.localScale = transform.localScale / 2;
+                    Ammo.SetActive(true);
+                    reloadTimer = 0;
+
+                }
+
+                else if (Input.GetKeyDown(KeyCode.X) || Input.GetKeyDown(KeyCode.LeftControl))
+                {
+                    GameObject Ammo = ObjectPool.SharedInstance.GetAmmo();
+                    Ammo.transform.position = ammoSpawnpointFront.transform.position;
+                    Ammo.transform.rotation = ammoSpawnpointFront.transform.rotation;
+                    Ammo.transform.localScale = transform.localScale / 2;
+                    Ammo.SetActive(true);
+                    reloadTimer = 0;
+                }
 
             }
 
-            else if (Input.GetKeyDown(KeyCode.X) || Input.GetKeyDown(KeyCode.LeftControl))
+
+
+
+            if (grounded == false && rb.velocity.y < 0)
             {
-                GameObject Ammo = ObjectPool.SharedInstance.GetAmmo();
-                Ammo.transform.position = ammoSpawnpointFront.transform.position;
-                Ammo.transform.rotation = ammoSpawnpointFront.transform.rotation;
-                Ammo.transform.localScale = transform.localScale/2;
-                Ammo.SetActive(true);
-                reloadTimer = 0;
+                rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y - fallSpeed, rb.velocity.z);
             }
-            
+
+            if (Jumped == true && coyoteTimer > coyoteMaxTime)
+            {
+                grounded = false;
+            }
         }
-
-
-        
-
-        if(grounded == false && rb.velocity.y < 0)
+        else
         {
-            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y - fallSpeed, rb.velocity.z);
+            print("Stunned!");
+            currentStunTime += Time.deltaTime;
+            if (currentStunTime >= totalStunTime)
+            {
+                isStunned = false;
+                currentStunTime = 0;
+            }
         }
-
-        if(Jumped == true && coyoteTimer > coyoteMaxTime)
-        {
-            grounded = false;
-        }
-
-
-
     }
-
-
 
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Platform")
         {
             grounded = true;
-            Jumped = false;
-            
+            Jumped = false;            
+        }
+
+        if (collision.gameObject.tag == "Enemy")
+        {
+            print("Touched by enemy");
+            isStunned = true;
+
+            float playerPosition = collision.transform.position.x;
+            float direction = 0;
+
+            if (playerPosition < transform.position.x)
+                direction = 1;
+            else if (playerPosition > transform.position.x)
+                direction = -1;
+
+            rb.velocity = new Vector3(0,0,0);
+            rb.AddForce(new Vector3(direction * knockBackForce, rb.velocity.y, rb.velocity.z));
         }
     }
 
@@ -191,4 +222,5 @@ public class PlayerMovement : MonoBehaviour
             coyoteTimer = 0;
         }
     }
+        
 }
