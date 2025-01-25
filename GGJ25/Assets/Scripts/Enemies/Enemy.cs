13d75f7aad83d3236b7349f0fa4b637123ae28ca
juumyllyla <31961349+juumyllyla@users.noreply.github.com>
2025-01-25 18:53:00 +0200

    Improved enemy movement

--- GGJ25/Assets/Scripts/Enemies/Enemy.cs
+++ GGJ25/Assets/Scripts/Enemies/Enemy.cs
@@ -72,12 +72,12 @@ public class Enemy : MonoBehaviour
 
         if (transform.rotation.eulerAngles.y == 90)
         {
-            rigidbody.velocity = new Vector3(patrolSpeed * Time.deltaTime, 0, 0);
+            rigidbody.velocity = new Vector3(patrolSpeed, 0, 0);
             //print("Moving right with " + rigidbody.velocity + " speed");
         }
         else if (transform.rotation.eulerAngles.y == 270)
         {
-            rigidbody.velocity = new Vector3(-1 * patrolSpeed * Time.deltaTime, 0, 0);
+            rigidbody.velocity = new Vector3(-1 * patrolSpeed, 0, 0);
             //print("Moving left with " + rigidbody.velocity + " speed");
         }
         else
@@ -148,7 +148,7 @@ public class Enemy : MonoBehaviour
         else if (playerPosition > transform.position.x)
             direction = 1;
 
-        rigidbody.velocity = new Vector3(direction * attackSpeed * Time.deltaTime, 0, 0);
+        rigidbody.velocity = new Vector3(direction * attackSpeed, 0, 0);
     }
 
     private void CountMercyTime()
